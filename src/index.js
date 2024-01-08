045613db5a91ca84cf3073b1acc7747135f2ee55
Alex Wen <alexwen.creates@gmail.com>
2024-01-07 20:03:14 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,19 +11,19 @@ const {
 const { isEnvironmentValid } = require('./helper')
 
 exports.initializeAndTrack = (location) => {
-  const options = window.gatsbyPluginGDPRCookiesOptions
+  // const options = window.gatsbyPluginGDPRCookiesOptions
 
-  if (isEnvironmentValid(options.environments)) {
-    if (location === undefined || location === null) {
-      console.error('Please provide a reach router location to the initializeAndTrack function.')
-    } else {
-      initializeAndTrackGoogleAnalytics(options.googleAnalytics, location)
-      initializeAndTrackGoogleTagManager(options.googleTagManager, location)
-      initializeAndTrackFacebookPixel(options.facebookPixel)
-      initializeAndTrackTikTokPixel(options.tikTokPixel)
-      initializeAndTrackHotjar(options.hotjar)
-      initializeChatwoot(options.chatwoot)
-      initializeLinkedin(options.linkedin)
-    }
-  }
+  // if (isEnvironmentValid(options.environments)) {
+  //   if (location === undefined || location === null) {
+  //     console.error('Please provide a reach router location to the initializeAndTrack function.')
+  //   } else {
+  //     initializeAndTrackGoogleAnalytics(options.googleAnalytics, location)
+  //     initializeAndTrackGoogleTagManager(options.googleTagManager, location)
+  //     initializeAndTrackFacebookPixel(options.facebookPixel)
+  //     initializeAndTrackTikTokPixel(options.tikTokPixel)
+  //     initializeAndTrackHotjar(options.hotjar)
+  //     initializeChatwoot(options.chatwoot)
+  //     initializeLinkedin(options.linkedin)
+  //   }
+  // }
 }
